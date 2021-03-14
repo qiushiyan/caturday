1a09df52440542d2e1bd5c1431a73ed31be48bbd
qiushi <qiushi.yann@gmail.com>
2021-03-14 15:04:15 +0800

    clear react setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { resolve } from "path"
 
+
+const srcPath = __dirname.endsWith("client") ? "./src" : "./client/src"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   resolve: {
     alias: [
-      { find: "@", replacement: resolve(__dirname, './client/src') },
+      { find: "@", replacement: resolve(__dirname, srcPath) },
     ]
   },
 })
